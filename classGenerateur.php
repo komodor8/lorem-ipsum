c8c94a1235705619ac13bd1e70ac8e8d386db85d
komodor8 <k.modor@yahoo.com>
2017-01-23 17:00:51 +0100

    mapper

---
+++ classGenerateur.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+class Generateur {
+	private 
+
+
+/*	private $paragraphes;
+	private $caracteres;
+	private $mots;
+	private $listes;
+	}
+
+	public function __construct ($p, $c, $m, $l) {
+		$this->$paragraphes = $p;
+		$this->$caracteres = $c;
+		$this->$mots = $m;
+		$this->$listes = $l;
+	}*/
+
+
+
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Generateur de Lorem Ipsum</title>
+</head>
+<body>
+<p> Comment generer un lorem ipsum ? </p>
+
+<input type="submit" name="generateur" value="generateur de Lorem" />
+
+<?php 
+
+require_once "mapper.php";
+
+	generer();
+?>
+</body>
+</html>
